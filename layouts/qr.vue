515100a213f325647cfaab892f5e071516c47ad0
sdurighello <stefano.durighello@gmail.com>
2019-02-09 17:05:31 +0100

    qr-layout

---
+++ layouts/qr.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="wrapper">
+            <nuxt />
+            <div class="clear" />
+            <div class="push" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {
+    }
+}
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+  margin: 0;
+}
+
+.clear {
+  clear: both;
+}
+
+/* sticky footer stuff */
+.wrapper {
+  min-height: 100vh;
+  margin-bottom: -60px;
+}
+
+.footer,
+.push {
+  height: 50px;
+  margin-top: 10px;
+}
+</style>
