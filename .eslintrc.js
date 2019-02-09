c686a8bcca5c3b9f88d2cd40a5666259e494c0b1
sdurighello <stefano.durighello@gmail.com>
2019-02-09 12:18:33 +0100

    linting and pug otherwise i go mad

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
-  extends: [
-    '@nuxtjs'
-  ],
-  // add your custom rules here
-  rules: {}
-}
+    root: true,
+    env: {
+      browser: true,
+      node: true
+    },
+    parserOptions: {
+      parser: 'babel-eslint'
+    },
+    extends: [
+      '@nuxtjs',
+      'plugin:vue/essential'
+    ],
+    // add your custom rules here
+    rules: {
+      'no-param-reassign': 0,
+      'no-multi-assign': 0,
+      'indent': ["error", 4, { "SwitchCase": 1 }],
+      'import/first': 0,
+      'no-console': 0,
+      'no-tabs': 0,
+      'vue/html-indent': ["error", 4]
+    }
+  }
+  
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,74 +1,86 @@
 const pkg = require('./package')
 
-
 module.exports = {
-  mode: 'spa',
+    mode: 'spa',
 
-  /*
+    /*
   ** Headers of the page
   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
+    head: {
+        title: pkg.name,
+        script: [
+        ],
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css?family=Barlow:400,600|Playfair+Display:700'
+            }
+        ]
+    },
 
-  /*
+    /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+    loading: { color: '#fff' },
 
-  /*
+    /*
   ** Global CSS
   */
-  css: [
-  ],
+    css: [
+        '~/assets/main.css'
+    ],
 
-  /*
+    /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+    plugins: [
+    ],
 
-  /*
+    /*
   ** Nuxt.js modules
   */
-  modules: [
+    modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios',
-    // Doc: https://bootstrap-vue.js.org/docs/
-    'bootstrap-vue/nuxt'
-  ],
-  /*
+        '@nuxtjs/axios'
+    ],
+    /*
   ** Axios module configuration
   */
-  axios: {
+    axios: {
     // See https://github.com/nuxt-community/axios-module#options
-  },
+    },
 
-  /*
+    /*
   ** Build configuration
   */
-  build: {
-    /*
+    build: {
+        transpile: [/^element-ui/],
+        babel: {
+            plugins: [
+                '@babel/transform-async-to-generator',
+                '@babel/transform-runtime'
+            ]
+        },
+
+        /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+        extend(config, ctx) {
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
